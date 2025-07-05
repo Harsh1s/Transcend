4f8bc9d1f6b8da8a0d99fe23ea1a9cbb8acb8d58
Harsh1s <harsh.raj.cd.ece21@itbhu.ac.in>
2025-07-05 19:43:25 +0530

    Detect type annotation tokens

--- src/analysis/ast_summary.h
+++ src/analysis/ast_summary.h
@@ -6,3 +6,4 @@
 namespace transcend::analysis {
 struct SymbolSummary { std::string name; std::string kind; int line; };
 std::vector<SymbolSummary> collect_exported_symbols(const std::string& source);
+bool looks_like_type_annotation(const std::string& token);
